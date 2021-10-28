57b913d6180d6207d6c13a34df56cdc866472469
Leinonen Henri Matias <henri.m.leinonen@student.jyu.fi>
2021-10-28 22:12:11 +0300

    file-tehtäviä

---
+++ osa04-Osa04_02.FileManager/src/main/java/filemanager/FileController.java
@@ -0,0 +1,67 @@
+package filemanager;
+
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class FileController {
+    
+    @Autowired
+    private FileRepository fileRepository; 
+
+    @GetMapping("/files")
+    public String show(Model model){
+        model.addAttribute("files", this.fileRepository.findAll());
+        return "files";
+    }
+
+    @PostMapping("/files/{id}")
+    public String delete(@PathVariable Long id) throws IOException {
+        this.fileRepository.delete(this.fileRepository.getOne(id));
+        return "redirect:/";
+    }
+    
+    
+    @GetMapping("/files/{id}")
+    public ResponseEntity<byte[]> download(Model model, @PathVariable Long id) {
+        
+        FileObject fo = this.fileRepository.getOne(id);
+        model.addAttribute("file.id", fo.getId());
+        model.addAttribute("file.name", fo.getName());
+        model.addAttribute("file.contentLength", fo.getContentLength());
+        model.addAttribute("file.contentType", fo.getContentType());
+        
+        FileObject fo2 = fileRepository.getOne(id);
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType(fo2.getContentType()));
+        headers.setContentLength(fo2.getContentLength());
+        
+        headers.add("Content-Disposition", "attachment; filename=" + fo2.getName());
+        
+        return new ResponseEntity<>(fo.getContent(), headers, HttpStatus.CREATED);
+    }
+    
+    @PostMapping("/files")
+    public String save(@RequestParam("file") MultipartFile file) throws IOException {
+        FileObject fo = new FileObject();
+        
+        fo.setName(file.getOriginalFilename());
+        fo.setContentType(file.getContentType());
+        fo.setContentLength(file.getSize());
+        fo.setContent(file.getBytes());
+        fileRepository.save(fo);
+        
+        return "redirect:/files";
+    }
+}
---
+++ osa04-Osa04_02.FileManager/src/main/java/filemanager/FileObject.java
@@ -0,0 +1,29 @@
+package filemanager;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Lob;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+/**
+ *
+ * @author Henri
+ */
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class FileObject extends AbstractPersistable<Long> {
+    
+    private String name;
+    private String contentType;
+    private Long contentLength;
+   
+    @Lob
+    @Basic(fetch = FetchType.LAZY)
+    private byte[] content;
+}
